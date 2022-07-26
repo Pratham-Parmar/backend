582f6988ebe424d792a29f50ffc2c5faa7f05dd3
PrathamParmar <parmarpratham1@gmail.com>
2022-07-26 23:19:41 +0530

    changes N Update

--- crm/settings.py
+++ crm/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 LOGIN_URL = "/login"
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -112,6 +113,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_URL = "/static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
